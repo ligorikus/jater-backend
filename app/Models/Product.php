40755506a62c2b49d8c389aefa1826a71c9e4c6c
semal55 <semal551@gmail.com>
2018-10-14 14:57:32 +0600

    init

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'rent_price'
+    ];
+
+    protected $guarded = [
+        'id'
+    ];
+}
